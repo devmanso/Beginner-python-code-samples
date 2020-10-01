aee83e5a8c8e8ae9e7237f2dd780d67928c434bb
Planebagels <64249106+Planebagels@users.noreply.github.com>
2020-10-01 19:05:42 -0400

    added files

---
+++ age_adder.py
@@ -0,0 +1,5 @@
+print("what is your age");
+Key = input();
+print("you will be " + str(int(Key) + 1) + " in one year");
+
+# this code will prompt you for your age, then will tell you what age you will be in one year
